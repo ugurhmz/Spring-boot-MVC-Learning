4762086bd58945c863827b9b9640665fa6b34135
Ugurhamzaoglu67 <craxx67@gmail.com>
2021-07-17 03:42:05 +0300

    Departmen Entity editing

--- springcomplex/src/main/java/com/ugurhmz/springcomplex/data/entity/Department.java
+++ springcomplex/src/main/java/com/ugurhmz/springcomplex/data/entity/Department.java
@@ -3,11 +3,17 @@ package com.ugurhmz.springcomplex.data.entity;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+@Entity
 public class Department {
 
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long departmentId;
 	private String departmentName;
 	
